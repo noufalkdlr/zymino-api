f7de52693544d16ffc7c7fba2267cd9a4ce06032
Noufal <noufalkakdlr@gmail.com>
2026-02-22 16:33:19 +0530

    dockerize

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 from dotenv import load_dotenv
 from datetime import timedelta
 
@@ -115,10 +116,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL", f"sqlite:///{BASE_DIR / 'db.sqlite3'}")
+    )
 }
 
 
--- reviews/schema.py
+++ reviews/schema.py
@@ -3,9 +3,9 @@ from drf_spectacular.utils import (
     extend_schema,
     extend_schema_view,
     inline_serializer,
-    OpenApiParameter,
 )
 from .serializers import ClientLookupSerializer
+
 TAG_VIEWSET_SCHEMA = extend_schema_view(
     list=extend_schema(
         tags=["Tags"],
